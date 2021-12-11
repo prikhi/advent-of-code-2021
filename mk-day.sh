0bf91423c1f271ead1151aeb719650c2a23b1e5c
Pavan Rikhi <pavan.rikhi@gmail.com>
2021-12-11 02:00:05 -0500

    Add Imports & Comment Separators to Template File

--- mk-day.sh
+++ mk-day.sh
@@ -20,15 +20,22 @@ cat >> "Day${DAY}.hs" <<EOF
 module Day${DAY} where
 
 import Control.Monad
-import Data.Maybe
+import Data.Array   (Array)
+import Data.Bifunctor
+import Data.Char
 import Data.Either
+import Data.Map     (Map)
+import Data.Maybe
+import Data.Set     (Set)
 import Text.ParserCombinators.ReadP
 
 import Harness
 import ParseHelper
 
+import qualified Data.Array as A
 import qualified Data.List as L
-import qualified GHC.Arr as A
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 import Debug.Trace
 
@@ -36,4 +43,16 @@ import Debug.Trace
 -- (parseInput lineParser) OR (parseInputRaw fullInputParser)
 main :: IO ()
 main = getInputAndSolve (const "Implement Parser") (const "Implement") (const "Implement")
+
+
+-- PARSE
+
+
+
+-- SOLVE
+
+
+
+-- HELPERS
+
 EOF
